833a25a3df591ead2234e4fd80b016b5a8dbae4d
Alexandro T. Netto <alex.t.netto@gmail.com>
2026-03-15 10:58:31 -0300

    feat: replace custom avatar with Thorin Avatar

--- apps/frontend/src/components/shared/EnsAvatar.tsx
+++ apps/frontend/src/components/shared/EnsAvatar.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Avatar } from '@ensdomains/thorin'
 import { useEnsName, useEnsAvatar } from 'wagmi'
 import makeBlockie from 'ethereum-blockies-base64'
 
@@ -8,12 +9,10 @@ interface EnsAvatarProps {
   size?: number
 }
 
-const Img = styled.img<{ $size: number }>`
+const Wrapper = styled.div<{ $size: number }>`
   width: ${({ $size }) => $size}px;
   height: ${({ $size }) => $size}px;
-  border-radius: 50%;
   flex-shrink: 0;
-  object-fit: cover;
 `
 
 export function EnsAvatar({ address, name, size = 32 }: EnsAvatarProps) {
@@ -21,15 +20,16 @@ export function EnsAvatar({ address, name, size = 32 }: EnsAvatarProps) {
     address: address as `0x${string}`,
     query: { enabled: !name },
   })
-
   const ensName = name ?? resolvedName ?? undefined
-
   const { data: avatarUrl } = useEnsAvatar({
     name: ensName,
     query: { enabled: !!ensName },
   })
-
   const src = avatarUrl ?? makeBlockie(address)
 
-  return <Img $size={size} src={src} alt={ensName ?? address} />
+  return (
+    <Wrapper $size={size}>
+      <Avatar label={ensName ?? address} src={src} shape="circle" noBorder />
+    </Wrapper>
+  )
 }
